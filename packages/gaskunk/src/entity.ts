616b6f44fde037168f8163960a1cfcf9b77faf6d
shuta13 <hollystarsun@gmail.com>
2021-03-14 19:17:16 +0900

    wip

--- packages/gaskunk/src/entity.ts
+++ packages/gaskunk/src/entity.ts
@@ -1,24 +1,28 @@
 export class Entity {
-  private sheets: GoogleAppsScript.Spreadsheet.Sheet | null = null;
-
-  constructor() {
-    this.sheets = SpreadsheetApp.getActiveSheet();
+  private create(
+    sheets: GoogleAppsScript.Spreadsheet.Spreadsheet,
+    name: string
+  ) {
+    sheets.insertSheet(name);
   }
-
-  create(tableName: string) {
-    // TODO: Insert new sheet
-    // console.log(this);
-  }
-  clear(tableName: string) {
-    // TODO: Delete sheet
+  clear(sheets: GoogleAppsScript.Spreadsheet.Spreadsheet, name: string) {
+    const target = sheets.getSheetByName(name);
+    if (target) {
+      sheets.deleteSheet(target);
+    }
+    return new Error(`Cannot clear ${name}`);
   }
   save() {
-    // TODO: Create table(data)
+    const sheets = SpreadsheetApp.getActiveSpreadsheet();
+
+    /**
+     * Insert sheet as new table
+     */
     const tableName = this.constructor.name;
-    console.log(tableName);
+    this.create(sheets, tableName);
 
     const columnNameAndInitialValues = Object.entries(this);
-    columnNameAndInitialValues.map((value) => {
+    columnNameAndInitialValues.forEach((value) => {
       /**
        * value[0] : column name
        * value[1] : initial data
