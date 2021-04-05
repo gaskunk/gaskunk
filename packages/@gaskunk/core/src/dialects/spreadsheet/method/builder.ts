d90c370531f0a1d860cb91325cb61715127671f8
shuta13 <hollystarsun@gmail.com>
2021-04-06 03:05:08 +0900

    update: add methods

--- packages/@gaskunk/core/src/dialects/spreadsheet/method/builder.ts
+++ packages/@gaskunk/core/src/dialects/spreadsheet/method/builder.ts
@@ -1,15 +1,119 @@
 export class SpreadsheetMethodBuilder<T> {
-  private _response: T[];
+  private _tableName: string;
+  private _response: T[] | null;
 
-  get build() {
-    return this._response;
+  async build() {
+    if (this._response) return this._response;
+    else throw new Error('failed to build method chain, response is empty');
   }
 
   constructor(tableName: string, options: any = {}) {
-    this._response = [];
+    this._tableName = tableName;
+    this._response = null;
   }
 
   select(arg: keyof T) {
     return this;
   }
+
+  as() {
+    return this;
+  }
+
+  column() {
+    return this;
+  }
+
+  from() {
+    return this;
+  }
+
+  with() {
+    this._tableName;
+    return this;
+  }
+
+  where() {
+    return this;
+  }
+
+  join() {
+    return this;
+  }
+
+  on() {
+    return this;
+  }
+
+  clear() {
+    return this;
+  }
+
+  having() {
+    return this;
+  }
+
+  distinct() {
+    return this;
+  }
+
+  group() {
+    return this;
+  }
+
+  limit() {
+    return this;
+  }
+
+  offset() {
+    return this;
+  }
+
+  union() {
+    return this;
+  }
+
+  order() {
+    return this;
+  }
+
+  insert() {
+    return this;
+  }
+
+  ifConflictOn() {
+    return this;
+  }
+
+  update() {
+    return this;
+  }
+
+  delete() {
+    return this;
+  }
+
+  truncate() {}
+
+  transaction() {}
+
+  count() {
+    return this;
+  }
+
+  min() {}
+
+  max() {}
+
+  sum() {}
+
+  avg() {}
+
+  increment() {}
+
+  decrement() {}
+
+  pluck() {}
+
+  first() {}
 }
